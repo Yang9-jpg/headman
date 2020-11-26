779469363d9ef268950dc20c527a6c84e75a5d53
阳阳 <2820857431@qq.com>
2020-11-26 13:33:13 +0800

    删除多余页面

--- backstage/src/views/Home.vue
+++ backstage/src/views/Home.vue
@@ -1,18 +1,26 @@
+<!-- 登录页 --> 
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- main -->
+  <div class="main">
+    <!-- content -->
+    <div class="content">
+      111
+      <br />
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+<style scoped>
+html,body {
+  width: 100%;
+  height: 100%;
+  background: url(../assets/img/1.jpg) no-repeat center center;
+  background-attachment: fixed; /*设置图片固定，不跟据页面的拉动而拉动*/
+  background-size: cover;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+</style>
+ 
